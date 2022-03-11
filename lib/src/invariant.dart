f7aeff6c6c96591b57629cad2de6606a7c47910f
arman <armantorkzaban@icloud.com>
2022-03-11 12:16:25 +0000

    wip - flattenReduce

--- lib/src/invariant.dart
+++ lib/src/invariant.dart
@@ -1,8 +1,8 @@
 import 'package:turf/turf.dart';
 
-/// Unwraps a coordinate from a [Point], [Feature<Point>], and a [Position].
+/// Unwraps a coordinate from a Point, Feature<Point>, and a Position.
 /// 
-/// gets [Position], [Point], and [Feature<Point>] and returns [Position].
+/// gets [Position], [Point], and [Feature]<Point> and returns [Position].
 /// For example:
 ///
 /// ```dart
@@ -26,10 +26,10 @@ Position getCoord(dynamic coord) {
   throw Exception("coord must be GeoJSON Point or Position");
 }
 
-/// Unwrap coordinates from a [Feature], [GeometryObject] or a [List]
+/// Unwrap coordinates from a [Feature], [GeometryObject] or a List
 /// 
-/// Gets a [List<dynamic>], [GeometryObject] or a [Feature] or a [List<dynamic>] and
-/// returns [List<dynamic>].
+/// Gets a List<dynamic>, [GeometryObject] or a [Feature] or a List<dynamic> and
+/// returns List<dynamic>.
 /// For example:
 /// ```dart
 /// var polygon = Polygon(coordinates: [
--- lib/src/meta.dart
+++ lib/src/meta.dart
@@ -166,192 +166,67 @@ void featureEach(GeoJSONObject geoJSON, FeatureEachCallback callback) {
   }
 }
 
-/// Callback for coordEach
-typedef CoordEachCallback = dynamic Function(
-    Position currentCoord, // or List<num>?
-    num coordIndex,
-    num featureIndex,
-    num multiFeatureIndex,
-    num geomtgeometryIndex);
 
-/// Iterates over coordinates of any [GeoJSONObject], calling [callback] on each
-/// iteration. Similar to [Iterable].forEach()
+
+/// Callback for flattenReduce
 ///
-/// Gets [FeatureCollection], [Feature], [GeometryObject] or any [GeoJSONOBject]
+/// Gets [previousValue] The accumulated value previously returned in the last invocation
+/// of the callback, or [initialValue], if supplied.
+/// /**
+/// * Callback for flattenReduce
+/// *
+/// * The first time the callback function is called, the values provided as arguments depend
+/// * on whether the reduce method has an initialValue argument.
+/// *
+/// * If an initialValue is provided to the reduce method:
+/// *  - The previousValue argument is initialValue.
+/// *  - The currentValue argument is the value of the first element present in the array.
+/// *
+/// * If an initialValue is not provided:
+/// *  - The previousValue argument is the value of the first element present in the array.
+/// *  - The currentValue argument is the value of the second element present in the array.
+/// */
+/// [Feature] currentFeature The current Feature being processed.
+/// [num] featureIndex The current index of the [Feature] being processed.
+/// [num] multiFeatureIndex The current index of the Multi-Feature being processed.
+///
+typedef FlattenReduceCallback = Function(
+    dynamic previousValue, // or an initialValue
+    Feature currentFeature,
+    num featureIndex,
+    num multiFeatureIndex);
 
-/*
-/**
-
- * @name coordEach
- * @param {FeatureCollection|Feature|Geometry} geojson any GeoJSON object
- * @param {Function} callback a method that takes (currentCoord, coordIndex, featureIndex, multiFeatureIndex)
- * @param {boolean} [excludeWrapCoord=false] whether or not to include the final coordinate of LinearRings that wraps the ring in its iteration.
- * @returns {void}
- * @example
- * var features = turf.featureCollection([
- *   turf.point([26, 37], {"foo": "bar"}),
- *   turf.point([36, 53], {"hello": "world"})
- * ]);
- *
- * turf.coordEach(features, function (currentCoord, coordIndex, featureIndex, multiFeatureIndex, geometryIndex) {
- *   //=currentCoord
- *   //=coordIndex
- *   //=featureIndex
- *   //=multiFeatureIndex
- *   //=geometryIndex
- * });
- */
-export function coordEach(geojson, callback, excludeWrapCoord) {
-  // Handles null Geometry -- Skips this GeoJSON
-  if (geojson === null) return;
-  var j,
-    k,
-    l,
-    geometry,
-    stopG,
-    coords,
-    geometryMaybeCollection,
-    wrapShrink = 0,
-    coordIndex = 0,
-    isGeometryCollection,
-    type = geojson.type,
-    isFeatureCollection = type === "FeatureCollection",
-    isFeature = type === "Feature",
-    stop = isFeatureCollection ? geojson.features.length : 1;
-
-  // This logic may look a little weird. The reason why it is that way
-  // is because it's trying to be fast. GeoJSON supports multiple kinds
-  // of objects at its root: FeatureCollection, Features, Geometries.
-  // This function has the responsibility of handling all of them, and that
-  // means that some of the `for` loops you see below actually just don't apply
-  // to certain inputs. For instance, if you give this just a
-  // Point geometry, then both loops are short-circuited and all we do
-  // is gradually rename the input until it's called 'geometry'.
-  //
-  // This also aims to allocate as few resources as possible: just a
-  // few numbers and booleans, rather than any temporary arrays as would
-  // be required with the normalization approach.
-  for (var featureIndex = 0; featureIndex < stop; featureIndex++) {
-    geometryMaybeCollection = isFeatureCollection
-      ? geojson.features[featureIndex].geometry
-      : isFeature
-      ? geojson.geometry
-      : geojson;
-    isGeometryCollection = geometryMaybeCollection
-      ? geometryMaybeCollection.type === "GeometryCollection"
-      : false;
-    stopG = isGeometryCollection
-      ? geometryMaybeCollection.geometries.length
-      : 1;
-
-    for (var geomIndex = 0; geomIndex < stopG; geomIndex++) {
-      var multiFeatureIndex = 0;
-      var geometryIndex = 0;
-      geometry = isGeometryCollection
-        ? geometryMaybeCollection.geometries[geomIndex]
-        : geometryMaybeCollection;
-
-      // Handles null Geometry -- Skips this geometry
-      if (geometry === null) continue;
-      coords = geometry.coordinates;
-      var geomType = geometry.type;
-
-      wrapShrink =
-        excludeWrapCoord &&
-        (geomType === "Polygon" || geomType === "MultiPolygon")
-          ? 1
-          : 0;
-
-      switch (geomType) {
-        case null:
-          break;
-        case "Point":
-          if (
-            callback(
-              coords,
-              coordIndex,
-              featureIndex,
-              multiFeatureIndex,
-              geometryIndex
-            ) === false
-          )
-            return false;
-          coordIndex++;
-          multiFeatureIndex++;
-          break;
-        case "LineString":
-        case "MultiPoint":
-          for (j = 0; j < coords.length; j++) {
-            if (
-              callback(
-                coords[j],
-                coordIndex,
-                featureIndex,
-                multiFeatureIndex,
-                geometryIndex
-              ) === false
-            )
-              return false;
-            coordIndex++;
-            if (geomType === "MultiPoint") multiFeatureIndex++;
-          }
-          if (geomType === "LineString") multiFeatureIndex++;
-          break;
-        case "Polygon":
-        case "MultiLineString":
-          for (j = 0; j < coords.length; j++) {
-            for (k = 0; k < coords[j].length - wrapShrink; k++) {
-              if (
-                callback(
-                  coords[j][k],
-                  coordIndex,
-                  featureIndex,
-                  multiFeatureIndex,
-                  geometryIndex
-                ) === false
-              )
-                return false;
-              coordIndex++;
-            }
-            if (geomType === "MultiLineString") multiFeatureIndex++;
-            if (geomType === "Polygon") geometryIndex++;
-          }
-          if (geomType === "Polygon") multiFeatureIndex++;
-          break;
-        case "MultiPolygon":
-          for (j = 0; j < coords.length; j++) {
-            geometryIndex = 0;
-            for (k = 0; k < coords[j].length; k++) {
-              for (l = 0; l < coords[j][k].length - wrapShrink; l++) {
-                if (
-                  callback(
-                    coords[j][k][l],
-                    coordIndex,
-                    featureIndex,
-                    multiFeatureIndex,
-                    geometryIndex
-                  ) === false
-                )
-                  return false;
-                coordIndex++;
-              }
-              geometryIndex++;
-            }
-            multiFeatureIndex++;
-          }
-          break;
-        case "GeometryCollection":
-          for (j = 0; j < geometry.geometries.length; j++)
-            if (
-              coordEach(geometry.geometries[j], callback, excludeWrapCoord) ===
-              false
-            )
-              return false;
-          break;
-        default:
-          throw new Error("Unknown Geometry Type");
-      }
+/// Reduce flattened features in any [GeoJSONObject], similar to [Iterable.reduce()].
+///
+/// Gets a [FeatureCollection], [Feature],[GeometryType], [GeoJSONObject],
+/// an optional [initialValue] Value to use as the first argument to the first
+/// call of the [FlattenReduceCallback], calls this [FlattenReduceCallback] method and
+/// returns a dynamic value that results from the reduction.
+/// For example:
+///
+/// var features = FeatureCollection([
+///     Feature(geometry: Point(coordiantes: Position.from([26, 37])), properties: {foo: 'bar'}),
+///     Feature(geommetry: MultiPoint(coordinates: [Position.from([40, 30])), Position.from([36, 53], {hello: 'world'})
+/// ]);
+///
+/// flattenReduce(features, function (previousValue, currentFeature, featureIndex, multiFeatureIndex) {
+///   //=previousValue
+///   //=currentFeature
+///   //=featureIndex
+///   //=multiFeatureIndex
+///   return currentFeature
+/// });
+///
+Feature flattenReduce(dynamic geojson, FlattenReduceCallback callback,
+    [dynamic initialValue]) {
+  var previousValue = initialValue;
+  flattenEach(geojson, (currentFeature, featureIndex, multiFeatureIndex) {
+    if (featureIndex == 0 && multiFeatureIndex == 0 && initialValue == null) {
+      previousValue = currentFeature;
+    } else {
+      previousValue = callback(
+          previousValue, currentFeature, featureIndex, multiFeatureIndex);
     }
-  }
-} 
-*/
+  });
+  return previousValue;
+}
