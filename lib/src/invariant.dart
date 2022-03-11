7622e2e7dcadc83bba2b6b1f43ab10ace38e306a
arman <armantorkzaban@icloud.com>
2022-03-11 07:39:52 +0000

    wrapped List in brackets

--- lib/src/invariant.dart
+++ lib/src/invariant.dart
@@ -26,9 +26,9 @@ Position getCoord(dynamic coord) {
   throw Exception("coord must be GeoJSON Point or Position");
 }
 
-/// Unwrap coordinates from a [Feature], [GeometryObject] or a List
+/// Unwrap coordinates from a [Feature], [GeometryObject] or a [List]
 /// 
-/// Gets a List<dynamic>, [GeometryObject] or a [Feature] or a List<dynamic> and
+/// Gets a List<dynamic>, [GeometryObject] or a [Feature] or a [List]<dynamic> and
 /// returns List<dynamic>.
 /// For example:
 /// ```dart
