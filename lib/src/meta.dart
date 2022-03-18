2ec159994f36216201eb09c4599815fdde1ebfb4
Arman Torkzaban <armantorkzaban@icloud.com>
2022-03-18 13:24:32 +0100

    flattenReduce WIP

--- lib/src/meta.dart
+++ lib/src/meta.dart
@@ -401,10 +401,9 @@ void featureEach(GeoJSONObject geoJSON, FeatureEachCallback callback) {
 typedef FlattenEachCallback = dynamic Function(
     Feature currentFeature, int featureIndex, int multiFeatureIndex);
 
-/// Iterate over flattened features in any [geoJSON] object, similar to
-/// Array.forEach, calling [callback] on each flattened feature
-
-///
+/// Iterate over flattened features in any [geoJSONObject], similar to
+/// [Iterate.forEach()], calling [callback] on each flattened feature
+///```dart
 /// flattenEach(featureCollection, (currentFeature, featureIndex, multiFeatureIndex) {
 ///   someOperationOnEachFeature(currentFeature);
 /// });
@@ -590,6 +589,64 @@ featureReduce(
       previousValue = currentFeature;
     } else {
       previousValue = callback(previousValue, currentFeature, featureIndex);
+    }
+  });
+  return previousValue;
+}
+
+/// Callback for flattenReduce
+/// The first time the callback function is called, the values provided as
+/// arguments depend on whether the reduce method has an [initialValue] argument.
+/// If an [initialValue] is provided to the reduce method:
+///  - The [previousValue] argument is initialValue.
+///  - The [currentValue] argument is the value of the first element present in the
+/// [List].
+/// If an [initialValue] is not provided:
+///  - The [previousValue] argument is the value of the first element present in
+/// the [List].
+///  - The [currentValue] argument is the value of the second element present in
+/// the [List].
+///
+/// flattenReduceCallback
+/// [previousValue] is the accumulated value previously returned in the
+/// last invocation of the callback, or [initialValue], if supplied.
+/// [currentFeature] is the current Feature being processed.
+/// [featureIndex] is the current index of the Feature being processed.
+/// [multiFeatureIndex] is the current index of the Multi-Feature being
+/// processed.
+typedef Feature FlattenReduceCallback(dynamic previousValue,
+    Feature currentFeature, int featureIndex, int multiFeatureIndex);
+
+/// Reduce flattened features in any [GeoJSONObject], similar to [Iterable.reduce()].
+/// Takes a [FeatureCollection], [Feature], or [Geometry]
+/// a [FlattenReduceCallback] method that takes (previousValue, currentFeature, featureIndex, multiFeatureIndex),
+/// an [initialValue] Value to use as the first argument to the first call of the callback.
+/// Returns the value that results from the reduction.
+/// For example:
+///
+/// ```dart
+/// var features = FeatureCollection(features: [
+///   Feature(geometry: Point(coordinates: Position.of([26, 37])), properties: {'foo': 'bar'}),
+///   Feature(geometry: Point(coordinates: Position.of([36, 53])), properties: {'foo': 'bar'})
+/// ]);
+///
+/// flattenReduce(features, (previousValue, currentFeature, featureIndex, multiFeatureIndex) {
+///   //=previousValue
+///   //=currentFeature
+///   //=featureIndex
+///   //=multiFeatureIndex
+///   return currentFeature
+/// });
+/// ```
+
+Feature flattenReduce(geojson, callback, initialValue) {
+  var previousValue = initialValue;
+  flattenEach(geojson, (currentFeature, featureIndex, multiFeatureIndex) {
+    if (featureIndex == 0 && multiFeatureIndex == 0 && initialValue == null) {
+      previousValue = currentFeature;
+    } else {
+      previousValue = callback(
+          previousValue, currentFeature, featureIndex, multiFeatureIndex);
     }
   });
   return previousValue;
