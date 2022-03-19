d1e9045d00b7ae744c93eb32a07605cf39760471
Lukas Himsel <lukas@himsel.me>
2022-03-19 13:08:22 +0100

    initial commit to segment file

---
+++ lib/src/line_segment.dart
@@ -0,0 +1,3 @@
+// TODO implement https://github.com/Turfjs/turf/blob/master/packages/turf-line-segment/index.ts
+// TODO implement https://github.com/Turfjs/turf/blob/fcdaa939c905e6cfa80cca11a0e7cbb851ad1a47/packages/turf-meta/index.js#L886
+// TODO implement https://github.com/Turfjs/turf/blob/fcdaa939c905e6cfa80cca11a0e7cbb851ad1a47/packages/turf-meta/index.js#L1001
