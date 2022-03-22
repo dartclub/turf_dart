e9840193ba4ea8b1fc5d9d45b40a6aa30b3725eb
armantorkzaban <armantorkzaban@icloud.com>
2022-03-22 21:48:24 +0100

    lineSegment()

--- lib/src/line_segment.dart
+++ lib/src/line_segment.dart
@@ -1,20 +1,11 @@
 // TODO implement https://github.com/Turfjs/turf/blob/master/packages/turf-line-segment/index.ts
 // TODO implement https://github.com/Turfjs/turf/blob/fcdaa939c905e6cfa80cca11a0e7cbb851ad1a47/packages/turf-meta/index.js#L886
 // TODO implement https://github.com/Turfjs/turf/blob/fcdaa939c905e6cfa80cca11a0e7cbb851ad1a47/packages/turf-meta/index.js#L1001
-import 'geojson.dart'
-    show
-        BBox,
-        Feature,
-        FeatureCollection,
-        LineString,
-        MultiLineString,
-        MultiPolygon,
-        Polygon;
-import 'invariant.dart' show getCoords;
-import 'meta.dart' show flattenEach;
+import 'geojson.dart';
+import 'invariant.dart';
+import 'meta.dart';
 
 // export default lineSegment;
-// import 'helpers.dart';
 
 /// Creates a [FeatureCollection] of 2-vertex [LineString] segments from a
 /// [LineString] or [MultiLineString] or [Polygon] and [MultiPolygon]
@@ -35,36 +26,51 @@ import 'meta.dart' show flattenEach;
 /// //addToMap
 /// var addToMap = [polygon, segments]
 
-FeatureCollection<LineString>
-    lineSegment<T extends LineString, MultiLineString, Polygon, MultiPolygon>(
-        Feature<T> geoJson) {
+FeatureCollection<LineString> lineSegment(GeoJSONObject geoJson) {
   List<Feature<LineString>> features = [];
+  if (geoJson is! LineString // todo: 2-vertex [LineString]?
+
+      ||
+      geoJson is! MultiLineString ||
+      geoJson is! MultiPolygon) {
+    throw Exception('Type is not supported');
+  } else {
+    flattenEach(geoJson,
+        (Feature currentFeature, int featureIndex, int multiFeatureIndex) {
+      // lineSegmentFeature(feature, results);
+    });
+  }
+
   return FeatureCollection(features: features);
 }
 
+
 /*
-lineSegment<T extends LineString, MultiLineString, Polygon,MultiPolygon>(
-  geojson: Feature<T> | FeatureCollection<G> | G): FeatureCollection<LineString> {
+function lineSegment<
+  G extends LineString | MultiLineString | Polygon | MultiPolygon
+>(
+  geojson: Feature<G> | FeatureCollection<G> | G
+): FeatureCollection<LineString> {
   if (!geojson) {
     throw new Error("geojson is required");
   }
 
-  const results= List<Feature<LineString>> [];
+  const results: Array<Feature<LineString>> = [];
   flattenEach(geojson, (feature: Feature<any>) => {
     lineSegmentFeature(feature, results);
   });
   return featureCollection(results);
 }
 
-
+/**
  * Line Segment
  *
  * @private
  * @param {Feature<LineString|Polygon>} geojson Line or polygon feature
  * @param {Array} results push to results
  * @returns {void}
- 
-lineSegmentFeature(
+ */
+function lineSegmentFeature(
   geojson: Feature<LineString | Polygon>,
   results: Array<Feature<LineString>>
 ) {
@@ -88,15 +94,15 @@ lineSegmentFeature(
   }
 }
 
-
+/**
  * Create Segments from LineString coordinates
  *
  * @private
  * @param {Array<Array<number>>} coords LineString coordinates
  * @param {*} properties GeoJSON properties
  * @returns {Array<Feature<LineString>>} line segments
- 
-createSegments(coords: number[][], properties: any) {
+ */
+function createSegments(coords: number[][], properties: any) {
   const segments: Array<Feature<LineString>> = [];
   coords.reduce((previousCoords, currentCoords) => {
     const segment = lineString([previousCoords, currentCoords], properties);
@@ -107,14 +113,15 @@ createSegments(coords: number[][], properties: any) {
   return segments;
 }
 
+/**
  * Create BBox between two coordinates (faster than @turf/bbox)
  *
  * @private
  * @param {Array<number>} coords1 Point coordinate
  * @param {Array<number>} coords2 Point coordinate
  * @returns {BBox} [west, south, east, north]
-
-bbox(coords1: number[], coords2: number[]): BBox {
+ */
+function bbox(coords1: number[], coords2: number[]): BBox {
   const x1 = coords1[0];
   const y1 = coords1[1];
   const x2 = coords2[0];
@@ -125,4 +132,6 @@ bbox(coords1: number[], coords2: number[]): BBox {
   const north = y1 > y2 ? y1 : y2;
   return [west, south, east, north];
 }
-*/
+
+export default lineSegment;
+ */
