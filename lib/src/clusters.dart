4c5eeadd74a8208bf05a3295b5ff3a21c54d2ccc
armantorkzaban <armantorkzaban@icloud.com>
2022-03-30 18:41:28 +0200

    Did the documentation

--- lib/src/clusters.dart
+++ lib/src/clusters.dart
@@ -2,8 +2,9 @@ import '../meta.dart';
 import '../helpers.dart';
 
 /// Get Cluster
-/// Takes a [FeatureCollection<Feature>] and a Filter used on GeoJSON properties to get Cluster.
-/// Returns [FeatureCollection] single cluster filtered by GeoJSON Properties
+/// Takes a [FeatureCollection<Feature>] and a [dynamic] [filter] used on GeoJSON properties
+/// to get Cluster.
+/// Returns a [FeatureCollection] single cluster filtered by GeoJSON Properties
 /// For example:
 ///
 /// ```dart
@@ -18,18 +19,18 @@ import '../helpers.dart';
 ///    ),
 ///    Feature(
 ///      geometry: Point(coordinates: Position.of([30, 30])),
-///      properties: {'marker-symbol': 'circle'},
+///      properties: {'marker-symbol': 'square'},
 ///    ),
 ///    Feature(
 ///      geometry: Point(coordinates: Position.of([40, 40])),
-///      properties: {'marker-symbol': 'circle'},
+///      properties: {'marker-symbol': 'triangle'},
 ///    ),
 ///  ]);
 ///
-/// // Create a cluster using K-Means (adds `cluster` to GeoJSON properties)
+/// // Creates a cluster using K-Means (adds `cluster` to GeoJSON properties)
 /// var clustered = clustersKmeans(geojson);
 ///
-/// // Retrieve first cluster (0)
+/// // Retrieves first cluster (0)
 /// var cluster = getCluster(clustered, {cluster: 0});
 /// //= cluster
 ///
@@ -49,36 +50,40 @@ FeatureCollection getCluster(FeatureCollection geojson, dynamic filter) {
   return FeatureCollection(features: features);
 }
 
+/// ClusterEachCallback
+/// Takes a [FeatureCollection], the cluster being processed, a [clusterValue]
+/// used to create cluster being processed, and the [currentIndex], the index of
+/// current element being processed in the [List]. Starts at index 0
+/// Returns void.
+typedef ClusterEachCallback = dynamic Function(
+  FeatureCollection? cluster,
+  dynamic clusterValue,
+  int? currentIndex,
+);
+
 /// clusterEach
-///
-/// @name clusterEach
-/// @param {FeatureCollection} geojson GeoJSON Features
-/// @param {string|number} property GeoJSON property key/value used to create clusters
-/// @param {Function} callback a method that takes (cluster, clusterValue, currentIndex)
-/// @returns {void}
+/// Takes a [FeatureCollection], a dynamic [property] key/value used to create clusters,
+/// and a [ClusterEachCallback] method that takes (cluster, clusterValue, currentIndex) and
+/// Returns void.
 /// For example:
 ///
 /// ```dart
 /// var geojson = FeatureCollection<Point>(features: [
 ///    Feature(
 ///      geometry: Point(coordinates: Position.of([10, 10])),
-///      properties: {'marker-symbol': 'circle'},
 ///    ),
 ///    Feature(
 ///      geometry: Point(coordinates: Position.of([20, 20])),
-///      properties: {'marker-symbol': 'circle'},
 ///    ),
 ///    Feature(
 ///      geometry: Point(coordinates: Position.of([30, 30])),
-///      properties: {'marker-symbol': 'circle'},
 ///    ),
 ///    Feature(
 ///      geometry: Point(coordinates: Position.of([40, 40])),
-///      properties: {'marker-symbol': 'circle'},
 ///    ),
 ///  ]);
 ///
-/// // Create a cluster using K-Means (adds `cluster` to GeoJSON properties)
+/// // Create a cluster using K-Means (adds `cluster` to [GeoJSONObject]'s properties)
 /// var clustered = clustersKmeans(geojson);
 ///
 /// // Iterates over each cluster
@@ -88,31 +93,18 @@ FeatureCollection getCluster(FeatureCollection geojson, dynamic filter) {
 ///     //= currentIndex
 /// })
 ///
-/// // Calculate the total number of clusters
+/// // Calculates the total number of clusters
 /// var total = 0
 /// clusterEach(clustered, 'cluster', function () {
 ///     total++;
 /// });
 ///
-/// // Create [List] of all the values retrieved from the 'cluster' property
+/// // Creates [List] of all the values retrieved from the 'cluster' property
 /// var values = []
 /// clusterEach(clustered, 'cluster', (cluster, clusterValue) {
-///     values.push(clusterValue);
+///     values.add(clusterValue);
 /// });
 /// ```
-
-/// ClusterEachCallback
-///
-/// Takes [FeatureCollection], the [cluster] being processed, a [clusterValue]
-/// used to create cluster being processed, and the [currentIndex], the index of
-/// current element being processed in the [List]. Starts at index 0
-/// Returns void.
-
-typedef ClusterEachCallback = dynamic Function(
-  FeatureCollection? cluster,
-  dynamic clusterValue,
-  int? currentIndex,
-);
 
 void clusterEach(
     FeatureCollection geojson, dynamic property, ClusterEachCallback callback) {
@@ -120,7 +112,7 @@ void clusterEach(
     throw Exception("property is required");
   }
 
-  // Create clusters based on property values
+  // Creates clusters based on property values
   var bins = createBins(geojson, property);
   var values = bins.keys.toList();
   for (var index = 0; index < values.length; index++) {
@@ -146,37 +138,48 @@ void clusterEach(
 ///  - The [previousValue] argument is the value of the first element present in the [List].
 ///  - The [currentValue] argument is the value of the second element present in the [List].
 ///
-/// @callback clusterReduceCallback
-/// @param {*} [previousValue] The accumulated value previously returned in the last invocation
-/// of the callback, or initialValue, if supplied.
-/// @param {FeatureCollection} [cluster] The current cluster being processed.
-/// @param {*} [clusterValue] Value used to create cluster being processed.
-/// @param {number} [currentIndex] The index of the current element being processed in the
-/// array. Starts at index 0, if an initialValue is provided, and at index 1 otherwise.
+/// Takes a [previousValue], the accumulated value previously returned in the last invocation
+/// of the callback, or [initialValue], if supplied, a [FeatureCollection] [cluster], the current
+/// cluster being processed, a [clusterValue] used to create cluster being processed and a
+/// [currentIndex], the index of the current element being processed in the
+/// [List].
+/// Starts at index 0, if an [initialValue] is provided, and at index 1 otherwise.
+typedef ClusterReduceCallback<T> = T? Function(
+  T? previousValue,
+  FeatureCollection? cluster,
+  dynamic clusterValue,
+  int? currentIndex,
+);
 
-/// Reduce clusters in GeoJSON Features, similar to Array.reduce()
+/// Reduces clusters in Features, similar to [Iterable.reduce]
+/// Takes a [FeatureCollection][geojson], a dynamic [porperty], a [GeoJSONObject]'s property key/value
+/// used to create clusters, a [ClusterReduceCallback] method, and an [initialValue] to
+/// use as the first argument to the first call of the callback.
+/// Returns the value that results from the reduction.
+/// For example:
 ///
-/// @name clusterReduce
-/// @param {FeatureCollection} geojson GeoJSON Features
-/// @param {string|number} property GeoJSON property key/value used to create clusters
-/// @param {Function} callback a method that takes (previousValue, cluster, clusterValue, currentIndex)
-/// @param {*} [initialValue] Value to use as the first argument to the first call of the callback.
-/// @returns {*} The value that results from the reduction.
-/// @example
-/// var geojson = featureCollection([
-///     turf.point([0, 0]),
-///     turf.point([2, 4]),
-///     turf.point([3, 6]),
-///     turf.point([5, 1]),
-///     turf.point([4, 2])
-/// ]);
+/// ```dart
+/// var geojson = FeatureCollection<Point>(features: [
+///    Feature(
+///      geometry: Point(coordinates: Position.of([10, 10])),
+///    ),
+///    Feature(
+///      geometry: Point(coordinates: Position.of([20, 20])),
+///    ),
+///    Feature(
+///      geometry: Point(coordinates: Position.of([30, 30])),
+///    ),
+///    Feature(
+///      geometry: Point(coordinates: Position.of([40, 40])),
+///    ),
+///  ]);
 ///
-/// // Create a cluster using K-Means (adds `cluster` to GeoJSON properties)
-/// var clustered = turf.clustersKmeans(geojson);
+/// // Creates a cluster using K-Means (adds `cluster` to GeoJSON properties)
+/// var clustered = clustersKmeans(geojson);
 ///
-/// // Iterate over each cluster and perform a calculation
+/// // Iterates over each cluster and perform a calculation
 /// var initialValue = 0
-/// turf.clusterReduce(clustered, 'cluster', function (previousValue, cluster, clusterValue, currentIndex) {
+/// clusterReduce(clustered, 'cluster', (previousValue, cluster, clusterValue, currentIndex) {
 ///     //=previousValue
 ///     //=cluster
 ///     //=clusterValue
@@ -184,23 +187,16 @@ void clusterEach(
 ///     return previousValue++;
 /// }, initialValue);
 ///
-/// // Calculate the total number of clusters
-/// var total = turf.clusterReduce(clustered, 'cluster', function (previousValue) {
+/// // Calculates the total number of clusters
+/// var total = clusterReduce(clustered, 'cluster', function (previousValue) {
 ///     return previousValue++;
 /// }, 0);
 ///
-/// // Create an Array of all the values retrieved from the 'cluster' property
-/// var values = turf.clusterReduce(clustered, 'cluster', function (previousValue, cluster, clusterValue) {
-///     return previousValue.concat(clusterValue);
+/// // Creates a [List] of all the values retrieved from the 'cluster' property.
+/// var values = clusterReduce(clustered, 'cluster', (previousValue, cluster, clusterValue){
+///     return previousValue.addAll(clusterValue);
 /// }, []);
-///
-
-typedef ClusterReduceCallback<T> = T? Function(
-  T? previousValue,
-  FeatureCollection? cluster,
-  dynamic clusterValue,
-  int? currentIndex,
-);
+/// ```
 
 T? clusterReduce<T>(
   FeatureCollection geojson,
@@ -220,23 +216,33 @@ T? clusterReduce<T>(
   return previousValue;
 }
 
+/// createBins
+/// Takes a [FeatureCollection] geojson, dynamic [property] values that are used
+/// to create bins.
+/// Returns a [Map<String, List<int>>] bins with Feature IDs
+/// For example
 ///
-/// Create Bins
-///
-/// @private
-/// @param {FeatureCollection} geojson GeoJSON Features
-/// @param {string|number} property Property values are used to create bins
-/// @returns {Object} bins with Feature IDs
-/// @example
-/// var geojson = turf.featureCollection([
-///     turf.point([0, 0], {cluster: 0, foo: 'null'}),
-///     turf.point([2, 4], {cluster: 1, foo: 'bar'}),
-///     turf.point([5, 1], {0: 'foo'}),
-///     turf.point([3, 6], {cluster: 1}),
-/// ]);
+/// ```dart
+/// /// var geojson = FeatureCollection<Point>(features: [
+///    Feature(
+///      geometry: Point(coordinates: Position.of([10, 10])),
+///      properties:{'cluster': 0, 'foo': 'null'},
+///    ),
+///    Feature(
+///      geometry: Point(coordinates: Position.of([20, 20])),
+///      properties: {'cluster': 1, 'foo': 'bar'},
+///    ),
+///    Feature(
+///      geometry: Point(coordinates: Position.of([30, 30])),
+///      properties: {'0': 'foo'},
+///    ),
+///    Feature(
+///      geometry: Point(coordinates: Position.of([40, 40])),
+///      properties: {'cluster': 1},
+///    ),
+///  ]);
 /// createBins(geojson, 'cluster');
 /// //= { '0': [ 0 ], '1': [ 1, 3 ] }
-///
 
 Map<String, List<int>> createBins(FeatureCollection geojson, dynamic property) {
   Map<String, List<int>> bins = {};
@@ -255,14 +261,9 @@ Map<String, List<int>> createBins(FeatureCollection geojson, dynamic property) {
   return bins;
 }
 
-///
-/// Apply Filter
-///
-/// @private
-/// @param {*} properties Properties
-/// @param {*} filter Filter
-/// @returns {boolean} applied Filter to properties
-///
+/// applyFilter
+/// Takes a [Map] [properties] and a [filter],
+/// Returns a [bool] indicating filter is applied to the properties.
 
 bool applyFilter(Map? properties, dynamic filter) {
   if (properties == null) return false;
@@ -278,19 +279,17 @@ bool applyFilter(Map? properties, dynamic filter) {
   }
 }
 
-///
 /// Properties contains filter (does not apply deepEqual operations)
+/// Takes a [Map] [properties] value, and a [Map] filter and
+/// Returns [bool] if filter does equal the [properties]
+/// For example
 ///
-/// @private
-/// @param {*} properties Properties
-/// @param {Object} filter Filter
-/// @returns {boolean} does filter equal Properties
-/// @example
+/// ```dart
 /// propertiesContainsFilter({foo: 'bar', cluster: 0}, {cluster: 0})
 /// //= true
 /// propertiesContainsFilter({foo: 'bar', cluster: 0}, {cluster: 1})
 /// //= false
-///
+/// ```
 bool propertiesContainsFilter(Map properties, Map filter) {
   var keys = filter.keys.toList();
   for (var i = 0; i < keys.length; i++) {
@@ -300,17 +299,16 @@ bool propertiesContainsFilter(Map properties, Map filter) {
   return true;
 }
 
+/// filterProperties
+/// Takes [Map<String, dynamic>] [properties], and [List<String>] [keys] used to
+/// filter Properties.
+/// Returns [Map<String, dynamic>] filtered Properties
+/// For example:
 ///
-/// Filter Properties
-///
-/// @private
-/// @param {*} properties Properties
-/// @param {Array<string>} keys Used to filter Properties
-/// @returns {*} filtered Properties
-/// @example
+/// ```dart
 /// filterProperties({foo: 'bar', cluster: 0}, ['cluster'])
 /// //= {cluster: 0}
-///
+/// ```
 
 Map<String, dynamic> filterProperties(
     Map<String, dynamic> properties, List<String> keys) {
