f75a87a4079bd50088cb8907a454446bd21083cd
Dennis Mwea <mweadennis2@gmail.com>
2022-04-06 09:07:41 +0300

    Add doc blocs and some more missing functions

--- lib/src/helpers.dart
+++ lib/src/helpers.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:turf/helpers.dart';
+
 enum Unit {
   meters,
   millimeters,
@@ -14,6 +16,7 @@ enum Unit {
   radians,
   degrees,
 }
+
 enum Grid {
   point,
   square,
@@ -130,8 +133,7 @@ num convertLength(
   return radiansToLength(lengthToRadians(length, originalUnit), finalUnit);
 }
 
-num convertArea(num area,
-    [originalUnit = Unit.meters, finalUnit = Unit.kilometers]) {
+num convertArea(num area, [originalUnit = Unit.meters, finalUnit = Unit.kilometers]) {
   if (area < 0) {
     throw Exception("area must be a positive number");
   }
@@ -147,4 +149,62 @@ num convertArea(num area,
   }
 
   return (area / startFactor) * finalFactor;
+}
+
+/// Creates a {@link Point} {@link Feature} from a Position.
+///
+/// @name point
+/// @param {Array<number>} coordinates longitude, latitude position (each in decimal degrees)
+/// @param {Object} [properties={}] an Object of key-value pairs to add as properties
+/// @param {Object} [options={}] Optional Parameters
+/// @param {Array<number>} [options.bbox] Bounding Box Array [west, south, east, north] associated with the Feature
+/// @param {string|number} [options.id] Identifier associated with the Feature
+/// @returns {Feature<Point>} a Point feature
+/// @example
+/// var point = turf.point([-75.343, 39.984]);
+///
+/// //=point
+Feature<Point> point(Position? coordinates, Map<String, dynamic> properties, {Map<String, dynamic>? options}) {
+  assert(coordinates != null);
+
+  final point = Point(coordinates: coordinates!);
+
+  return feature(point, properties, options: options);
+}
+
+/// Wraps a GeoJSON {@link Geometry} in a GeoJSON {@link Feature}.
+///
+/// @name feature
+/// @param {Geometry} geometry input geometry
+/// @param {Object} [properties={}] an Object of key-value pairs to add as properties
+/// @param {Object} [options={}] Optional Parameters
+/// @param {Array<number>} [options.bbox] Bounding Box Array [west, south, east, north] associated with the Feature
+/// @param {string|number} [options.id] Identifier associated with the Feature
+/// @returns {Feature} a GeoJSON Feature
+/// @example
+/// var geometry = {
+///   "type": "Point",
+///   "coordinates": [110, 50]
+/// };
+///
+/// var feature = turf.feature(geometry);
+///
+/// //=feature
+Feature<G> feature<G extends GeometryObject, P>(G geom, P? properties, {Map<String, dynamic>? options}) {
+  var feat = <String, dynamic>{};
+
+  if (options != null) {
+    if (options['id'] != null || options['id'] == 0) {
+      feat['id'] = options['id'];
+    }
+
+    if (options['bbox'] != null) {
+      feat['bbox'] = options['bbox'];
+    }
+  }
+
+  feat['properties'] = properties ?? <String, dynamic>{};
+  feat['geometry'] = geom;
+
+  return Feature.fromJson(feat);
 }
