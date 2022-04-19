9c8f9da820c9f04d99dc24ccf993a3eefd0b8ef8
Lukas Himsel <lukas@himsel.me>
2022-04-19 09:54:24 +0200

    Implement `nearestPointOn(Multi)Line` (#86)
    Co-authored-by: Levente Morva <levente95@gmail.com>

---
+++ lib/src/nearest_point_on_line.dart
@@ -0,0 +1,191 @@
+import 'dart:math';
+
+import 'bearing.dart';
+import 'destination.dart';
+import 'distance.dart';
+import 'geojson.dart';
+import 'helpers.dart';
+import 'intersection.dart';
+
+class _Nearest {
+  final Point point;
+  final num distance;
+  final int index;
+  final num location;
+
+  _Nearest({
+    required this.point,
+    required this.distance,
+    required this.index,
+    required this.location,
+  });
+
+  Feature<Point> toFeature() {
+    return Feature(
+      geometry: point,
+      properties: {
+        'dist': distance,
+        'index': index,
+        'location': location,
+      },
+    );
+  }
+}
+
+class _NearestMulti extends _Nearest {
+  final int line;
+
+  _NearestMulti({
+    required Point point,
+    required num distance,
+    required int index,
+    required num location,
+    required this.line,
+  }) : super(
+          point: point,
+          distance: distance,
+          index: index,
+          location: location,
+        );
+
+  @override
+  Feature<Point> toFeature() {
+    return Feature(
+      geometry: point,
+      properties: {
+        'dist': super.distance,
+        'line': line,
+        'index': super.index,
+        'location': super.location,
+      },
+    );
+  }
+}
+
+_Nearest _nearestPointOnLine(
+  LineString line,
+  Point point, [
+  Unit unit = Unit.kilometers,
+]) {
+  _Nearest? nearest;
+
+  num length = 0;
+
+  for (var i = 0; i < line.coordinates.length - 1; ++i) {
+    final startCoordinates = line.coordinates[i];
+    final stopCoordinates = line.coordinates[i + 1];
+
+    final startPoint = Point(coordinates: startCoordinates);
+    final stopPoint = Point(coordinates: stopCoordinates);
+
+    final sectionLength = distance(startPoint, stopPoint, unit);
+
+    final start = _Nearest(
+      point: startPoint,
+      distance: distance(point, startPoint, unit),
+      index: i,
+      location: length,
+    );
+
+    final stop = _Nearest(
+      point: stopPoint,
+      distance: distance(point, stopPoint, unit),
+      index: i + 1,
+      location: length + sectionLength,
+    );
+
+    final heightDistance = max(start.distance, stop.distance);
+    final direction = bearing(startPoint, stopPoint);
+
+    final perpendicular1 = destination(
+      point,
+      heightDistance,
+      direction + 90,
+      unit,
+    );
+
+    final perpendicular2 = destination(
+      point,
+      heightDistance,
+      direction - 90,
+      unit,
+    );
+
+    final intersectionPoint = intersects(
+      LineString.fromPoints(points: [perpendicular1, perpendicular2]),
+      LineString.fromPoints(points: [startPoint, stopPoint]),
+    );
+
+    _Nearest? intersection;
+
+    if (intersectionPoint != null) {
+      intersection = _Nearest(
+        point: intersectionPoint,
+        distance: distance(point, intersectionPoint, unit),
+        index: i,
+        location: length + distance(startPoint, intersectionPoint, unit),
+      );
+    }
+
+    if (nearest == null || start.distance < nearest.distance) {
+      nearest = start;
+    }
+
+    if (stop.distance < nearest.distance) {
+      nearest = stop;
+    }
+
+    if (intersection != null && intersection.distance < nearest.distance) {
+      nearest = intersection;
+    }
+
+    length += sectionLength;
+  }
+
+  // A `LineString` is guaranteed to have at least two points and thus a
+  // nearest point has to exist.
+
+  return nearest!;
+}
+
+_NearestMulti? _nearestPointOnMultiLine(
+  MultiLineString lines,
+  Point point, [
+  Unit unit = Unit.kilometers,
+]) {
+  _NearestMulti? nearest;
+
+  for (var i = 0; i < lines.coordinates.length; ++i) {
+    final line = LineString(coordinates: lines.coordinates[i]);
+
+    final candidate = _nearestPointOnLine(line, point);
+
+    if (nearest == null || candidate.distance < nearest.distance) {
+      nearest = _NearestMulti(
+        point: candidate.point,
+        distance: candidate.distance,
+        index: candidate.index,
+        location: candidate.location,
+        line: i,
+      );
+    }
+  }
+
+  return nearest;
+}
+
+Feature<Point> nearestPointOnLine(
+  LineString line,
+  Point point, [
+  Unit unit = Unit.kilometers,
+]) {
+  return _nearestPointOnLine(line, point, unit).toFeature();
+}
+
+Feature<Point>? nearestPointOnMultiLine(
+  MultiLineString lines,
+  Point point, [
+  Unit unit = Unit.kilometers,
+]) {
+  return _nearestPointOnMultiLine(lines, point, unit)?.toFeature();
+}
