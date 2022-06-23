f05529963b964acd1337de4e21c5efd34c013921
armantorkzaban <armantorkzaban@icloud.com>
2022-06-23 09:45:26 +0200

    test and impl. done for polygonToLine

--- test/components/polygon_to_line_test.dart
+++ test/components/polygon_to_line_test.dart
@@ -1,71 +1,72 @@
-const fs = require("fs");
-const test = require("tape");
-const path = require("path");
-const load = require("load-json-file");
-const write = require("write-json-file");
-const { point } = require("@turf/helpers");
-const { polygon } = require("@turf/helpers");
-const polygonToLine = require("./index").default;
+import 'dart:convert';
+import 'dart:io';
 
-const directories = {
-  in: path.join(__dirname, "test", "in") + path.sep,
-  out: path.join(__dirname, "test", "out") + path.sep,
-};
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import 'package:turf/polygon_to_line.dart';
+import 'package:turf/turf.dart';
 
-const fixtures = fs.readdirSync(directories.in).map((filename) => {
-  return {
-    filename,
-    name: path.parse(filename).name,
-    geojson: load.sync(directories.in + filename),
-  };
-});
+main() {
+  group(
+    'polygonToLine',
+    () {
+      var inDir = Directory('./test/examples/polygonToLine/in');
+      for (var file in inDir.listSync(recursive: true)) {
+        if (file is File && file.path.endsWith('.geojson')) {
+          test(
+            file.path,
+            () {
+              var inSource = file.readAsStringSync();
+              var inGeom = GeoJSONObject.fromJson(jsonDecode(inSource));
+              var results = polygonToLine(inGeom);
 
-test("turf-polygon-to-linestring", (t) => {
-  for (const { name, filename, geojson } of fixtures) {
-    const results = polygonToLine(geojson);
+              var outPath = './' +
+                  file.uri.pathSegments
+                      .sublist(0, file.uri.pathSegments.length - 2)
+                      .join('/') +
+                  '/out/${file.uri.pathSegments.last}';
 
-    if (process.env.REGEN) write.sync(directories.out + filename, results);
-    t.deepEqual(load.sync(directories.out + filename), results, name);
-  }
-  // Handle Errors
-  t.throws(() => polygonToLine(point([10, 5])), "throws - invalid geometry");
-  t.throws(() => polygonToLine(polygon([])), "throws - empty coordinates");
-  t.end();
-});
-
-/**
- * const fs = require("fs");
-const test = require("tape");
-const path = require("path");
-const load = require("load-json-file");
-const write = require("write-json-file");
-const { point } = require("@turf/helpers");
-const { polygon } = require("@turf/helpers");
-const polygonToLine = require("./index").default;
-
-const directories = {
-  in: path.join(__dirname, "test", "in") + path.sep,
-  out: path.join(__dirname, "test", "out") + path.sep,
-};
-
-const fixtures = fs.readdirSync(directories.in).map((filename) => {
-  return {
-    filename,
-    name: path.parse(filename).name,
-    geojson: load.sync(directories.in + filename),
-  };
-});
-
-test("turf-polygon-to-linestring", (t) => {
-  for (const { name, filename, geojson } of fixtures) {
-    const results = polygonToLine(geojson);
-
-    if (process.env.REGEN) write.sync(directories.out + filename, results);
-    t.deepEqual(load.sync(directories.out + filename), results, name);
-  }
-  // Handle Errors
-  t.throws(() => polygonToLine(point([10, 5])), "throws - invalid geometry");
-  t.throws(() => polygonToLine(polygon([])), "throws - empty coordinates");
-  t.end();
-});
- */
+              var outSource = File(outPath).readAsStringSync();
+              var outGeom = GeoJSONObject.fromJson(jsonDecode(outSource));
+              if (results is FeatureCollection) {
+                expect(outGeom is FeatureCollection, true);
+                for (var i = 0; i < results.features.length; i++) {
+                  expect(results.features[i],
+                      equals((outGeom as FeatureCollection).features[i]));
+                  expect(
+                    (results.features[i].geometry as GeometryType).coordinates,
+                    equals((outGeom.features[i].geometry as GeometryType)
+                        .coordinates),
+                  );
+                  expect(
+                    results.features[i].properties,
+                    equals(outGeom.features[i].properties),
+                  );
+                }
+              } else if (results is Feature) {
+                expect(outGeom is Feature, true);
+                expect((outGeom as Feature).properties,
+                    equals(results.properties));
+                expect((results.geometry as GeometryType).type,
+                    equals((outGeom.geometry)?.type));
+                expect((results.geometry as GeometryType).coordinates,
+                    equals((outGeom.geometry as GeometryType).coordinates));
+              }
+            },
+          );
+          test(
+            "handles error",
+            () {
+              // Handle Errors
+              expect(
+                  () => polygonToLine(Point(coordinates: Position.of([10, 5]))),
+                  throwsA(isA<Exception>()));
+              expect(() => polygonToLine(Polygon(coordinates: [])),
+                  throwsA(isA<RangeError>()));
+            },
+          );
+        }
+      }
+    },
+  );
+}
