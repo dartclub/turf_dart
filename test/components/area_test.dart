5993074a81cf0cee67dc45f885dbb29144b932b1
yarden <yarden.fishler@prospera.ag>
2022-07-23 10:48:21 +0300

    fixed the test and added a benchmark test

--- test/components/area_test.dart
+++ test/components/area_test.dart
@@ -3,11 +3,26 @@ import 'package:turf/src/geojson.dart';
 import 'package:turf/turf.dart';
 
 main() {
-  test('test intersects()', () {
+  test('test area()', () {
     Feature<Polygon> poly = Feature<Polygon>(
-      geometry: Polygon(coordinates: [[Position(125, -15), Position(113, -22), Position(154, -27), Position(144, -15), Position(125, -15)]]),
+      geometry: Polygon(coordinates: [
+        [
+          Position(125, -15),
+          Position(113, -22),
+          Position(117, -37),
+          Position(130, -33),
+          Position(148, -39),
+          Position(154, -27),
+          Position(144, -15),
+          Position(125, -15)
+        ]
+      ]),
     );
 
-    print(area(poly));
+    var areaResult = area(poly);
+
+    expect(areaResult, isNot(equals(null)));
+    final roundedResult = round(areaResult!);
+    expect(roundedResult, equals(7748891609977));
   });
 }
