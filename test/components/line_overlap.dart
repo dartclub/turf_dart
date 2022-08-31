26560178ec4b1f11b6ab49ce6693cb78b78cd381
armantorkzaban <armantorkzaban@icloud.com>
2022-08-31 13:13:56 +0200

    issue with test is worked on

--- test/components/line_overlap.dart
+++ test/components/line_overlap.dart
@@ -1,42 +1,66 @@
-const directories = {
-  in: path.join(__dirname, "test", "in") + path.sep,
-  out: path.join(__dirname, "test", "out") + path.sep,
-};
+import 'dart:convert';
+import 'dart:io';
 
-let fixtures = fs.readdirSync(directories.in).map((filename) => {
-  return {
-    filename,
-    name: path.parse(filename).name,
-    geojson: load.sync(directories.in + filename),
-  };
-});
-// fixtures = fixtures.filter(({name}) => name.includes('#901'));
+import 'package:test/test.dart';
+import 'package:turf/helpers.dart';
+import 'package:turf/src/line_overlap.dart';
+import 'package:turf/src/meta/feature.dart';
+import 'package:turf_equality/turf_equality.dart';
 
-test("turf-line-overlap", (t) => {
-  for (const { filename, name, geojson } of fixtures) {
-    const [source, target] = geojson.features;
-    const shared = colorize(
-      lineOverlap(source, target, geojson.properties),
+ 
+main(){
+  group('line_overlap function',(){
+    // fixtures = fixtures.filter(({name}) => name.includes('#901'));
+
+
+var inDir = Directory('./test/examples/line_overlaps/in');
+      for (var file in inDir.listSync(recursive: true)) {
+        if (file is File && file.path.endsWith('.geojson')) {
+          test(
+            file.path,
+            () {
+              var inSource = file.readAsStringSync();
+              var inGeom = GeoJSONObject.fromJson(jsonDecode(inSource));
+          
+              var outPath = './' +
+                  file.uri.pathSegments
+                      .sublist(0, file.uri.pathSegments.length - 2)
+                      .join('/') +
+                  '/out/${file.uri.pathSegments.last}';
+
+              var outSource = File(outPath).readAsStringSync();
+            
+              var outGeom = GeoJSONObject.fromJson(jsonDecode(outSource));
+
+
+                  FeatureCollection<LineString> results = lineOverlap((inGeom as FeatureCollection<LineString>).features.first, inGeom.features.last);
+              Equality eq = Equality();
+              expect(eq.compare(results, outGeom), true); 
+
+test("turf-line-overlap", () {
+
+     shared = colorize(
+      lineOverlap(source, target, tolerance: geojson.properties),
       "#0F0"
     );
-    const results = featureCollection(shared.features.concat([source, target]));
+    var
+     results = FeatureCollection(features: [...shared.features, source, target]);
 
-    if (process.env.REGEN) write.sync(directories.out + filename, results);
     t.deepEquals(results, load.sync(directories.out + filename), name);
   }
-  t.end();
 });
 
-test("turf-line-overlap - Geometry Object", (t) => {
-  const line1 = lineString([
-    [115, -35],
-    [125, -30],
-    [135, -30],
-    [145, -35],
+test("turf-line-overlap - Geometry Object", () {
+  var line1 = LineString(coordinates:[
+    Position.of([115, -35]),
+    Position.of([125, -30]),
+    Position.of([135, -30]),
+    Position.of([145, -35]),
   ]);
-  const line2 = lineString([
-    [135, -30],
-    [145, -35],
+  var
+   line2 = LineString(coordinates:[
+    Position.of([135, -30]),
+    Position.of([145, -35]),
   ]);
 
   t.true(
@@ -46,45 +70,51 @@ test("turf-line-overlap - Geometry Object", (t) => {
   t.end();
 });
 
-test("turf-line-overlap - multiple segments on same line", (t) => {
-  const line1 = lineString([
-    [0, 1],
-    [1, 1],
-    [1, 0],
-    [2, 0],
-    [2, 1],
-    [3, 1],
-    [3, 0],
-    [4, 0],
-    [4, 1],
-    [4, 0],
+test("turf-line-overlap - multiple segments on same line", () {
+  var
+   line1 = LineString(coordinates:[
+    Position.of([0, 1]),
+    Position.of([1, 1]),
+    Position.of([1, 0]),
+    Position.of([2, 0]),
+    Position.of([2, 1]),
+    Position.of([3, 1]),
+    Position.of([3, 0]),
+    Position.of([4, 0]),
+    Position.of([4, 1]),
+    Position.of([4, 0]),
   ]);
-  const line2 = lineString([
-    [0, 0],
-    [6, 0],
+  var
+   line2 = LineString(coordinates:[
+    Position.of([0, 0]),
+    Position.of([6, 0]),
   ]);
 
   t.true(
-    lineOverlap(line1.geometry, line2.geometry).features.length === 2,
+    lineOverlap(line1.geometry, line2.geometry).features.length == 2,
     "multiple segments on same line"
   );
   t.true(
-    lineOverlap(line2.geometry, line1.geometry).features.length === 2,
+    lineOverlap(line2.geometry, line1.geometry).features.length ==2,
     "multiple segments on same line - swapped order"
   );
   t.end();
 });
 
-function colorize(features, color = "#F00", width = 25) {
-  const results = [];
-  featureEach(features, (feature) => {
-    feature.properties = {
-      stroke: color,
-      fill: color,
-      "stroke-width": width,
-    };
-    results.push(feature);
-  });
-  if (features.type === "Feature") return results[0];
-  return featureCollection(results);
+ colorize(features, {color = "#F00", width = 25}) {
+  var
+   results = <Feature>[];
+  featureEach(features, (  Feature currentFeature,
+  int featureIndex) {
+    currentFeature.properties = {
+      'stroke': color,
+      'fill': color,
+      "stroke-width": width};
+    results.add(currentFeature);
+  },);
+  if (features is List<Feature>) return results[0];
+  return FeatureCollection(features: results);
+  }
+  },);
 }
+
