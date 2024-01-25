7c9a2e884520a72ed142a287076dd6b447f29756
Leif Linse <leif.linse@trivector.se>
2024-01-25 15:52:57 +0100

    Add test of length() using default unit

--- test/components/length_test.dart
+++ test/components/length_test.dart
@@ -3,29 +3,34 @@ import 'package:turf/helpers.dart';
 import 'package:turf/length.dart';
 
 void main() {
-  test('length', () {
-    final start = Position.named(
-      lat: 55.7090430186194,
-      lng: 13.184645393920405,
-    );
-    final via = Position.named(
-      lat: 55.70901279569489,
-      lng: 13.185546616182755,
-    );
-    final end = Position.named(
-      lat: 55.70764669578079,
-      lng: 13.187563637197076,
-    );
-    final line = Feature<LineString>(
-      geometry: LineString(
-        coordinates: [
-          start,
-          via,
-          end,
-        ],
-      ),
-    );
+  test('length - in meters', () {
     final len = length(line, Unit.meters);
     expect(len.round(), equals(254));
   });
+  test('length - default unit (km)', () {
+    final len = length(line);
+    expect((len * 1000).round(), equals(254));
+  });
 }
+
+final start = Position.named(
+  lat: 55.7090430186194,
+  lng: 13.184645393920405,
+);
+final via = Position.named(
+  lat: 55.70901279569489,
+  lng: 13.185546616182755,
+);
+final end = Position.named(
+  lat: 55.70764669578079,
+  lng: 13.187563637197076,
+);
+final line = Feature<LineString>(
+  geometry: LineString(
+    coordinates: [
+      start,
+      via,
+      end,
+    ],
+  ),
+);
