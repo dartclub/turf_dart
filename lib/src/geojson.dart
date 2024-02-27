b7090daad830b45fb2c121232764b8c8cce3e71d
Jonas Siedentop <Jonas@Sieden.top>
2024-02-27 22:17:31 +0100

    minor updates

--- lib/src/geojson.dart
+++ lib/src/geojson.dart
@@ -278,8 +278,6 @@ class Position extends CoordinateType {
 /// You can either specify 4 or 6 parameters
 /// If you are using the default constructor with two dimensional positions (lng + lat only), please use the constructor like this:
 /// `BBox(lng1, lat1, lng2, lat2);`
-
-// ToDo: different constructors for 2D and 3D positions!
 class BBox extends CoordinateType {
   BBox(
     /// longitude 1
@@ -685,7 +683,7 @@ class Feature<T extends GeometryObject> extends GeoJSONObject {
 
   @override
   Map<String, dynamic> toJson() => super.serialize({
-        if (id != null) 'id': id,
+        'id': id,
         'geometry': geometry!.toJson(),
         'properties': properties,
         ...fields,
