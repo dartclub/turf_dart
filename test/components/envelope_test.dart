59a90db773f46517bb37d1da2331be48e7304a91
NithyaNN3 <nithyaniranjani@gmail.com>
2025-04-03 01:02:57 +1100

    added envelope tests for multiline, polygon, multipolygon

--- test/components/envelope_test.dart
+++ test/components/envelope_test.dart
@@ -13,6 +13,58 @@ void main() {
     ])
   );
 
+  final poly = Feature<Polygon>(
+      geometry: Polygon(coordinates: [
+    [
+      Position.named(lat: 101.0, lng: 0.0),
+      Position.named(lat: 101.0, lng: 1.0),
+      Position.named(lat: 100.0, lng: 1.0),
+      Position.named(lat: 100.0, lng: 0.0),
+      Position.named(lat: 101.0, lng: 0.0),
+    ],
+  ]));
+
+  final multiLine = Feature<MultiLineString>(
+      geometry: MultiLineString(coordinates: [
+    [
+      Position.named(lat: 100.0, lng: 0.0),
+      Position.named(lat: 101.0, lng: 1.0),
+    ],
+    [
+      Position.named(lat: 102.0, lng: 2.0),
+      Position.named(lat: 103.0, lng: 3.0),
+    ],
+  ]));
+
+  final multiPoly = Feature<MultiPolygon>(
+      geometry: MultiPolygon(coordinates: [
+    [
+      [
+        Position.named(lat: 102.0, lng: 2.0),
+        Position.named(lat: 103.0, lng: 2.0),
+        Position.named(lat: 103.0, lng: 3.0),
+        Position.named(lat: 102.0, lng: 3.0),
+        Position.named(lat: 102.0, lng: 2.0),
+      ],
+    ],
+    [
+      [
+        Position.named(lat: 100.0, lng: 0.0),
+        Position.named(lat: 101.0, lng: 0.0),
+        Position.named(lat: 101.0, lng: 1.0),
+        Position.named(lat: 100.0, lng: 1.0),
+        Position.named(lat: 100.0, lng: 0.0),
+      ],
+      [
+        Position.named(lat: 100.2, lng: 0.2),
+        Position.named(lat: 100.8, lng: 0.2),
+        Position.named(lat: 100.8, lng: 0.8),
+        Position.named(lat: 100.2, lng: 0.8),
+        Position.named(lat: 100.2, lng: 0.2),
+      ],
+    ],
+  ]));
+
   test("envelope for point", () {
     // Point
     final pointEnvelope = envelope(point);
@@ -55,5 +107,67 @@ void main() {
     );
   });
 
-  
+  test("envelope for polygon", () {
+    // Point
+    final polyEnvelope = envelope(poly);
+    expect(
+      polyEnvelope,
+      equals(Feature<Polygon>(
+        geometry: Polygon(coordinates: [
+          [
+            Position.named(lat: 100.0, lng: 0.0),
+            Position.named(lat: 101.0, lng: 0.0),
+            Position.named(lat: 101.0, lng: 1.0),
+            Position.named(lat: 100.0, lng: 1.0),
+            Position.named(lat: 100.0, lng: 0.0),
+          ]
+        ])
+      )),
+      reason: "polygon",
+    );
+  });
+
+  test("envelope for multilinestring", () {
+    // MultiLineString
+    final multiLineEnvelope = envelope(multiLine);
+
+    // Directly use the expected envelope in the expect call
+    expect(
+      multiLineEnvelope,
+      equals(Feature<Polygon>(
+        geometry: Polygon(coordinates: [
+          [
+            Position.named(lat: 100.0, lng: 0.0),
+            Position.named(lat: 103.0, lng: 0.0),
+            Position.named(lat: 103.0, lng: 3.0),
+            Position.named(lat: 100.0, lng: 3.0),
+            Position.named(lat: 100.0, lng: 0.0),
+          ]
+        ]),
+      )),
+      reason: "MultiLineString",
+    );
+  });
+
+  test("envelope for multipolygon", () {
+    // MultiPolygon
+    final multiPolyEnvelope = envelope(multiPoly);
+
+    // Directly use the expected envelope in the expect call
+    expect(
+      multiPolyEnvelope,
+      equals(Feature<Polygon>(
+        geometry: Polygon(coordinates: [
+          [
+            Position.named(lat: 100.0, lng: 0.0),
+            Position.named(lat: 103.0, lng: 0.0),
+            Position.named(lat: 103.0, lng: 3.0),
+            Position.named(lat: 100.0, lng: 3.0),
+            Position.named(lat: 100.0, lng: 0.0),
+          ]
+        ]),
+      )),
+      reason: "MultiPolygon",
+    );
+  });
 }
