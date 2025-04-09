a2702167df2002dccc4846dbf83df712f10379e8
Dean Papas <deanpapas@bigpond.com>
2025-04-09 19:17:55 +1000

    Feedback Changes

--- lib/src/square.dart
+++ lib/src/square.dart
@@ -3,17 +3,17 @@ import 'package:turf/turf.dart';
 
 /// Takes a [BBox] and returns a square [BBox] of equal dimensions.
 BBox square(BBox bbox) {
-  var horizontalDistance = distance(
+  final horizontalDistance = distance(
     Point(coordinates: Position.named(lng: bbox.lng1, lat: bbox.lat1)),
     Point(coordinates: Position.named(lng: bbox.lng2, lat: bbox.lat1))
   );
-  var verticalDistance = distance(
+  final verticalDistance = distance(
     Point(coordinates: Position.named(lng: bbox.lng1, lat: bbox.lat1)),
     Point(coordinates: Position.named(lng: bbox.lng1, lat: bbox.lat2))
   );
 
   if (horizontalDistance >= verticalDistance) {
-    var verticalMidpoint = (bbox.lat1 + bbox.lat2) / 2;
+    final verticalMidpoint = (bbox.lat1 + bbox.lat2) / 2;
     return BBox.named(
       lng1: bbox.lng1,
       lat1: verticalMidpoint - (bbox.lng2 - bbox.lng1) / 2,
@@ -21,7 +21,7 @@ BBox square(BBox bbox) {
       lat2: verticalMidpoint + (bbox.lng2 - bbox.lng1) / 2,
     );
   } else {
-    var horizontalMidpoint = (bbox.lng1 + bbox.lng2) / 2;
+    final horizontalMidpoint = (bbox.lng1 + bbox.lng2) / 2;
     return BBox.named(
       lng1: horizontalMidpoint - (bbox.lat2 - bbox.lat1) / 2,
       lat1: bbox.lat1,
