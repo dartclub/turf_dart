3cdca733b2c0435b9361ad06b4d6508f8398e22a
Dean Papadofrangakis <106540468+deanpapas@users.noreply.github.com>
2025-04-15 18:15:54 +1000

    Implement polygonTangent (#212)
    * Implement polygonTangent
    
    * Feedback Changes
    
    * Feedback Changes v2

--- lib/turf.dart
+++ lib/turf.dart
@@ -31,6 +31,7 @@ export 'nearest_point.dart';
 export 'point_to_line_distance.dart';
 export 'point_on_feature.dart';
 export 'polygon_smooth.dart';
+export 'polygon_tangents.dart';
 export 'polygon_to_line.dart';
 export 'polyline.dart';
 export 'square.dart';
