3dbd25ea86b673574444521be50a5e189db4ed8b
tiinsy <s3900481@student.rmit.edu.au>
2025-06-02 12:11:32 +1000

    added comments to implementation of center_of_mass

--- lib/src/center_of_mass.dart
+++ lib/src/center_of_mass.dart
@@ -4,24 +4,28 @@ import 'centroid.dart';
 // Takes a Feature and returns its center of mass using the Centroid of Polygon formula
 // Link to formula: https://en.wikipedia.org/wiki/Centroid#Centroid_of_polygon
 Feature<Point> centerOfMass(
-  GeoJSONObject geoJson,
-  {Map<String, dynamic>? properties = const {}, }) 
-
-  {
-  List<Position> coords =[];
+  GeoJSONObject geoJson, {
+  Map<String, dynamic>? properties = const {},
+}) {
+  List<Position> coords = [];
 
   coordEach(
     geoJson,
-    (Position? currentCoord, int? coordIndex, int? featureIndex, int? multiFeatureIndex, int? geometryIndex) {
-      if (currentCoord != null && currentCoord[0] != null && currentCoord[1] != null) {
+    (Position? currentCoord, int? coordIndex, int? featureIndex,
+        int? multiFeatureIndex, int? geometryIndex) {
+      if (currentCoord != null &&
+          currentCoord[0] != null &&
+          currentCoord[1] != null) {
         coords.add(currentCoord);
       }
     },
- );
+  );
 
+  // Find centre of geoJSON object
   Feature<Point> centre = centroid(geoJson);
   Position translation = centre.geometry!.coordinates;
 
+  // Set up areas with pre-defined variables
   double sx = 0;
   double sy = 0;
   double sArea = 0;
@@ -38,10 +42,10 @@ Feature<Point> centerOfMass(
     double xi = pi[0]!.toDouble(), yi = pi[1]!.toDouble();
     double xj = pj[0]!.toDouble(), yj = pj[1]!.toDouble();
 
-    double a = xi * yj - xj * yi; 
-    sArea += a;                  
-    sx += (xi + xj) * a;         
-    sy += (yi + yj) * a;         
+    double a = xi * yj - xj * yi;
+    sArea += a;
+    sx += (xi + xj) * a;
+    sy += (yi + yj) * a;
   }
 
   if (sArea == 0) {
@@ -50,12 +54,12 @@ Feature<Point> centerOfMass(
 
   double areaFactor = 1 / (6 * sArea);
   List<double> finalCoordinates = [
-  translation[0]! + areaFactor * sx,
-  translation[1]! + areaFactor * sy,
+    translation[0]! + areaFactor * sx,
+    translation[1]! + areaFactor * sy,
   ];
   // returns Point
   return Feature<Point>(
-    geometry: Point(coordinates: Position(finalCoordinates[0], finalCoordinates[1])),
-    properties: properties
-  );
+      geometry: Point(
+          coordinates: Position(finalCoordinates[0], finalCoordinates[1])),
+      properties: properties);
 }
